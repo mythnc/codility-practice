46cbc84378608f34eb615d9843ec6eae408f4c4a
N.C Wu <mythnc@livemail.tw>
2015-12-09 22:59:23 +0800

    lesson2 - MaxCounters 100%

---
+++ lesson2/MaxCounters.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+def solution(N, A):
+    s = [0] * N
+    max_ = base = 0
+    after_max_counter = False
+    for n in A:
+        if 1 <= n <= N:
+            if s[n-1] < base:
+                s[n-1] = base + 1
+            else:
+                s[n-1] += 1
+            if s[n-1] > max_:
+                max_ = s[n-1]
+        elif n == N + 1:
+            base = max_
+
+    for i, n in enumerate(s):
+        if n < base:
+            s[i] = base
+
+    return s
+
+if __name__ == '__main__':
+    a = [3, 4, 4, 6, 1, 4, 4]
+    print solution(5, a)
+    a = [3, 4, 4, 6, 1, 4, 4, 6]
+    print solution(5, a)
+    a = [3]
+    print solution(5, a)
